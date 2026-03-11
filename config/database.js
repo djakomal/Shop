d248b3a37f63f82a71e4faea563c9823b852edf9
DjakoMal <rafioudjakomallo00@gmail.com>
2026-03-11 12:03:01 +0000

    modification du point env

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME     || 'boutique_alimentaire',
-  process.env.DB_USER     || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME    ,
+  process.env.DB_USER  ,
+  process.env.DB_PASSWORD ,
   {
-    host:    process.env.DB_HOST || 'localhost',
-    port:    parseInt(process.env.DB_PORT) || 3306,
+    host:    process.env.DB_HOST ,
+    port:    parseInt(process.env.DB_PORT) ,
     dialect: 'mysql',
     logging: (msg) => {
       // Log uniquement en dev et uniquement les vraies requêtes (pas les SELECT 1)
